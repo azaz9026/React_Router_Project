bf2aa0557c84149ad5d9ba6ef585664c368282a8
Md Azaz <mdazaz9026@gmail.com>
2023-09-22 19:08:44 +0530

    Layout and outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,9 @@ function App() {
   return (
     <>
 
-      
+      <Header />
+      <Home />
+      <Footer />
 
     </>
   )
